df4eeba3bd368e7a25b0bc51bc0e3e096913fef3
Zachery Bergman <bzach634@gmail.com>
2021-03-06 11:39:37 -0700

    Add basic translate_letter test and method

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,3 +1,13 @@
 class Dictionary
+  attr_reader :dictionary
   
+  def initialize
+    @dictionary = {
+      "a" => "b"
+    }
+  end
+
+  def translate_letter(letter)
+    @dictionary[letter]
+  end
 end
--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -1,10 +1,16 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/dictionary'
+require 'pry'
 
 class DictionaryTest < Minitest::Test
   def test_it_exists
     dictionary = Dictionary.new
     assert_instance_of Dictionary, dictionary
+  end
+
+  def test_it_can_find_braille_translation_for_letter
+    dictionary = Dictionary.new
+    assert_equal "b", dictionary.translate_letter("a")
   end
 end
