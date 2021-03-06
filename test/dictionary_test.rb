b0205c6436f1228a2ec20fb1bf4cd285eab6c068
Zachery Bergman <bzach634@gmail.com>
2021-03-06 11:32:01 -0700

    Create Dictionary class and test

---
+++ test/dictionary_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/dictionary'
+
+class DictionaryTest < Minitest::Test
+  def test_it_exists
+    dictionary = Dictionary.new
+    assert_instance_of Dictionary, dictionary
+  end
+end
